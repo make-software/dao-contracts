86849d6f8c75712b54ed84eca1ae5f28ef5605e0
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-03-22 07:21:23 +0100

    call  method with args

--- macros/src/casper_contract.rs
+++ macros/src/casper_contract.rs
@@ -1,9 +1,8 @@
 use convert_case::Casing;
-use proc_macro2::{Ident, Span, TokenStream};
+use proc_macro2::TokenStream;
 use quote::{format_ident, quote, TokenStreamExt};
-use syn::{punctuated::Punctuated, token, Token, TraitItemMethod};
 
-use crate::{contract::utils::collect_arg_idents, parser::ContractTrait};
+use crate::{contract::utils, parser::ContractTrait};
 
 pub fn generate_macro(input: &ContractTrait) -> TokenStream {
     let macro_ident = format_ident!(
@@ -47,7 +46,7 @@ fn generate_interface_methods(contract: &ContractTrait) -> TokenStream {
     let mut methods = TokenStream::new();
     methods.append_all(contract.methods.iter().map(|method| {
         let ident = &method.sig.ident;
-        let (casper_args, punctuated_args) = parse_args(method);
+        let (casper_args, punctuated_args) = utils::parse_casper_args(method);
 
         quote! {
             #[no_mangle]
@@ -59,22 +58,4 @@ fn generate_interface_methods(contract: &ContractTrait) -> TokenStream {
         }
     }));
     methods
-}
-
-fn parse_args(method: &TraitItemMethod) -> (TokenStream, Punctuated<Ident, Token![,]>) {
-    let comma = token::Comma([Span::call_site()]);
-
-    let mut punctuated_args: Punctuated<Ident, Token![,]> = Punctuated::new();
-    let mut casper_args = TokenStream::new();
-
-    collect_arg_idents(method).iter().for_each(|ident| {
-        punctuated_args.push_value(format_ident!("{}", ident));
-        punctuated_args.push_punct(comma);
-
-        casper_args.append_all(quote! {
-            let #ident = casper_contract::contract_api::runtime::get_named_arg(stringify!(#ident));
-        });
-    });
-
-    (casper_args, punctuated_args)
 }
