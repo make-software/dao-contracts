56ee94117fb5f727026aa04ef34527a287b8ee2f
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-03-22 08:22:49 +0100

    unit test a contract initial args

--- utils/sample-contract/src/contract.rs
+++ utils/sample-contract/src/contract.rs
@@ -3,7 +3,7 @@
 
 #[casper_dao_utils::casper_dao_macros::casper_contract_interface]
 pub trait ImportantContractInterface {
-    fn init(&mut self);
+    fn init(&mut self, first_arg: casper_types::U256, second_arg: casper_types::U256);
     fn mint(&mut self, recipient: casper_dao_utils::Address, amount: casper_types::U256);
     fn balance_of(&mut self, to: casper_dao_utils::Address) -> casper_types::U256;
 }
@@ -12,7 +12,7 @@ pub trait ImportantContractInterface {
 pub struct ImportantContract {}
 
 impl ImportantContractInterface for ImportantContract {
-    fn init(&mut self) {}
+    fn init(&mut self, first_arg: casper_types::U256, second_arg: casper_types::U256) {}
 
     fn mint(&mut self, recipient: casper_dao_utils::Address, amount: casper_types::U256) {}
 
