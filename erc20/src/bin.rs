085e403dbac0cfa97ef9d3f4f73d451601c2ea32
Maciej Zieliński <maciek.s.zielinski@gmail.com>
2022-03-24 01:22:08 +0100

    ERC20 Contract

---
+++ erc20/src/bin.rs
@@ -0,0 +1,4 @@
+use erc20::{ERC20, ERC20Interface};
+erc20::erc_20!();
+
+fn main() {}
---
+++ erc20/src/tests.rs
@@ -0,0 +1,18 @@
+use casper_dao_utils::TestEnv;
+use casper_types::U256;
+use erc20::ERC20Test;
+
+static NAME: &str = "Plascoin";
+static SYMBOL: &str = "PLS";
+static DECIMALS: u8 = 2;
+
+// fn setup(initial_supply: U256) -> ERC20Test {
+//     // let token = ERC20Test::new(&TestEnv::new());
+// }
+
+#[test]
+fn test_erc20() {
+    assert_eq!(1, 2);
+}
+
+fn main() {}
