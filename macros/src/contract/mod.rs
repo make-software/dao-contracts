0896a219ef9d95b781f11ba44cd0419f93628df2
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-03-24 14:43:38 +0100

    Rename file
    casper_contract_interface.rs -> generator.rs

--- macros/src/contract/mod.rs
+++ macros/src/contract/mod.rs
@@ -1,9 +1,9 @@
 mod caller;
 mod casper_contract;
-mod casper_contract_interface;
 mod contract;
 mod contract_test;
+mod generator;
 mod parser;
 mod utils;
-pub use casper_contract_interface::generate_code;
+pub use generator::generate_code;
 pub use parser::CasperContractItem;
