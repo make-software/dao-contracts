dfe8860e69de8c03069ef96785b199e4df1143de
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-03-25 15:27:54 +0100

    Move sample-contract and test to dao-macros module
    * remove unused ui tests
    * update template code
    * move module and tests from dao-utils

--- dao-macros/tests/expand_tests.rs
+++ dao-macros/tests/expand_tests.rs
@@ -5,18 +5,6 @@ use std::{
 };
 
 #[test]
-#[ignore]
-fn tests() {
-    let test_cases = trybuild::TestCases::new();
-    test_cases.pass("tests/01-parse.rs");
-    test_cases.pass("tests/02-contract-impl.rs");
-    test_cases.pass("tests/03-create-caller.rs");
-    test_cases.pass("tests/04-entry-points.rs");
-    test_cases.pass("tests/05-caller-impl-interface.rs");
-    test_cases.pass("tests/06-contract-test-interface.rs");
-}
-
-#[test]
 fn check_casper_contract_interface_output() {
     let (expansion, template) = expand(
         &["expand", "--lib", "contract", "--features", "test-support"],
