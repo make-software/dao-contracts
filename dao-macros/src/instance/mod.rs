4819517eb2b67507a7d314e57501f64d8350d391
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-03-30 14:44:16 +0200

    apply suggestions

--- dao-macros/src/instance/mod.rs
+++ dao-macros/src/instance/mod.rs
@@ -19,12 +19,21 @@ fn generate_or_err(input: DeriveInput) -> Result<TokenStream, syn::Error> {
 }
 
 fn parse_data(struct_ident: Ident, data_struct: DataStruct) -> Result<TokenStream, syn::Error> {
-    let fields: Vec<TokenStream> = data_struct.fields.into_iter().map(|field| {
-        let ident = field.ident.unwrap();
-        quote! {
-            #ident: casper_dao_utils::instance::Instanced::instance(&format!("{}:{}", stringify!(#ident), namespace))
-        }
-    }).collect();
+    let fields: Vec<TokenStream> = data_struct
+        .fields
+        .into_iter()
+        .map(|field| {
+            let ident = field.ident.unwrap();
+            quote! {
+                #ident: casper_dao_utils::instance::Instanced::instance(if namespace.is_empty() {
+                    format!("{}", stringify!(#ident))
+                } else {
+                    format!("{}_{}", stringify!(#ident), namespace)
+                }
+                .as_str())
+            }
+        })
+        .collect();
 
     Ok(quote! {
         impl casper_dao_utils::instance::Instanced for #struct_ident {
@@ -41,17 +50,20 @@ fn parse_data(struct_ident: Ident, data_struct: DataStruct) -> Result<TokenStrea
 #[cfg(test)]
 mod tests {
     use quote::quote;
-    use syn::punctuated::Punctuated;
+    use syn::{
+        punctuated::Punctuated, token, DataStruct, Field, Fields, FieldsNamed, Token, Type,
+        VisPublic, Visibility,
+    };
 
     use super::parse_data;
 
     #[test]
     fn parsing_struct_data_works() {
-        let mut fields: Punctuated<syn::Field, syn::Token![,]> = Punctuated::new();
+        let mut fields: Punctuated<Field, Token![,]> = Punctuated::new();
         fields.push(create_field("b", syn::parse_quote! { B }));
         fields.push(create_field("c", syn::parse_quote! { C }));
         fields.push(create_field("d", syn::parse_quote! { D }));
-        let input: syn::DataStruct = build_struct(fields);
+        let input: DataStruct = build_struct(fields);
 
         let result = parse_data(quote::format_ident!("A"), input)
             .unwrap()
@@ -60,36 +72,50 @@ mod tests {
             impl casper_dao_utils::instance::Instanced for A {
                 fn instance(namespace: &str) -> Self {
                     Self {
-                        b: casper_dao_utils::instance::Instanced::instance(&format!("{}:{}", stringify!(b), namespace)),
-                        c: casper_dao_utils::instance::Instanced::instance(&format!("{}:{}", stringify!(c), namespace)),
-                        d: casper_dao_utils::instance::Instanced::instance(&format!("{}:{}", stringify!(d), namespace)),
+                        b: casper_dao_utils::instance::Instanced::instance(if namespace.is_empty() {
+                            format!("{}", stringify!(b))
+                        } else {
+                            format!("{}_{}", stringify!(b), namespace)
+                        }
+                        .as_str()),
+                        c: casper_dao_utils::instance::Instanced::instance(if namespace.is_empty() {
+                            format!("{}", stringify!(c))
+                        } else {
+                            format!("{}_{}", stringify!(c), namespace)
+                        }
+                        .as_str()),
+                        d: casper_dao_utils::instance::Instanced::instance(if namespace.is_empty() {
+                            format!("{}", stringify!(d))
+                        } else {
+                            format!("{}_{}", stringify!(d), namespace)
+                        }
+                        .as_str()),
                     }
                 }
             }
-        }.to_string();
+        }
+        .to_string();
 
         pretty_assertions::assert_eq!(expected, result);
     }
 
-    fn create_field(name: &str, ty: syn::Type) -> syn::Field {
-        syn::Field {
+    fn create_field(name: &str, ty: Type) -> Field {
+        Field {
             attrs: vec![],
             colon_token: Default::default(),
             ident: Some(quote::format_ident!("{}", name)),
-            vis: syn::Visibility::Public(syn::VisPublic {
+            vis: Visibility::Public(VisPublic {
                 pub_token: Default::default(),
             }),
             ty,
         }
     }
 
-    fn build_struct(
-        fields: syn::punctuated::Punctuated<syn::Field, syn::token::Comma>,
-    ) -> syn::DataStruct {
-        syn::DataStruct {
+    fn build_struct(fields: Punctuated<Field, token::Comma>) -> DataStruct {
+        DataStruct {
             struct_token: Default::default(),
             semi_token: Default::default(),
-            fields: syn::Fields::Named(syn::FieldsNamed {
+            fields: Fields::Named(FieldsNamed {
                 brace_token: Default::default(),
                 named: fields,
             }),
