dd5f2ee1b73078c8997e77c166bbb7522a195ef0
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-04-14 10:55:19 +0200

    Add AccessControl module * AccessControl combines Owner and Whitelist modules * Simplify contract by using AccessControl and delegate

--- dao-contracts/src/reputation.rs
+++ dao-contracts/src/reputation.rs
@@ -1,10 +1,11 @@
-use casper_dao_modules::{Owner, TokenWithStaking, Whitelist};
+use casper_dao_modules::{AccessControl, TokenWithStaking};
 use casper_dao_utils::{
     casper_dao_macros::{casper_contract_interface, Instance},
     casper_env::caller,
     Address,
 };
 use casper_types::U256;
+use delegate::delegate;
 
 // TODO: Put it lower.
 //
@@ -127,60 +128,48 @@ pub trait ReputationContractInterface {
 #[derive(Instance)]
 pub struct ReputationContract {
     pub token: TokenWithStaking,
-    pub owner: Owner,
-    pub whitelist: Whitelist,
+    pub access_control: AccessControl,
 }
 
 impl ReputationContractInterface for ReputationContract {
+    delegate! {
+        to self.access_control {
+            fn change_ownership(&mut self, owner: Address);
+            fn add_to_whitelist(&mut self, address: Address);
+            fn remove_from_whitelist(&mut self, address: Address);
+            fn is_whitelisted(&self, address: Address) -> bool;
+            fn get_owner(&self) -> Option<Address>;
+        }
+    }
+
     fn init(&mut self) {
         let deployer = caller();
-        self.owner.init(deployer);
-        self.whitelist.add_to_whitelist(deployer);
+        self.access_control.init(deployer);
     }
 
     fn mint(&mut self, recipient: Address, amount: U256) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.token.mint(recipient, amount);
     }
 
     fn burn(&mut self, owner: Address, amount: U256) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.token.burn(owner, amount);
     }
 
     fn transfer_from(&mut self, owner: Address, recipient: Address, amount: U256) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.token.raw_transfer(owner, recipient, amount);
     }
 
-    fn change_ownership(&mut self, owner: Address) {
-        self.owner.ensure_owner();
-        self.owner.change_ownership(owner);
-        self.whitelist.add_to_whitelist(owner);
-    }
-
-    fn add_to_whitelist(&mut self, address: Address) {
-        self.owner.ensure_owner();
-        self.whitelist.add_to_whitelist(address);
-    }
-
-    fn remove_from_whitelist(&mut self, address: Address) {
-        self.owner.ensure_owner();
-        self.whitelist.remove_from_whitelist(address);
-    }
-
     fn stake(&mut self, address: Address, amount: U256) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.token.stake(address, amount);
     }
 
     fn unstake(&mut self, address: Address, amount: U256) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.token.unstake(address, amount);
-    }
-
-    fn get_owner(&self) -> Option<Address> {
-        self.owner.get_owner()
     }
 
     fn total_supply(&self) -> U256 {
@@ -189,10 +178,6 @@ impl ReputationContractInterface for ReputationContract {
 
     fn balance_of(&self, address: Address) -> U256 {
         self.token.balance_of(&address)
-    }
-
-    fn is_whitelisted(&self, address: Address) -> bool {
-        self.whitelist.is_whitelisted(&address)
     }
 
     fn get_staked_balance_of(&self, address: Address) -> U256 {
--- dao-contracts/src/variable_repository.rs
+++ dao-contracts/src/variable_repository.rs
@@ -1,4 +1,4 @@
-use casper_dao_modules::{Owner, Record, Repository, Whitelist};
+use casper_dao_modules::{AccessControl, Record, Repository};
 use casper_dao_utils::{
     casper_contract::unwrap_or_revert::UnwrapOrRevert,
     casper_dao_macros::{casper_contract_interface, Instance},
@@ -6,6 +6,7 @@ use casper_dao_utils::{
     Address, Error,
 };
 use casper_types::bytesrepr::{Bytes, FromBytes};
+use delegate::delegate;
 
 #[casper_contract_interface]
 pub trait VariableRepositoryContractInterface {
@@ -24,37 +25,29 @@ pub trait VariableRepositoryContractInterface {
 
 #[derive(Instance)]
 pub struct VariableRepositoryContract {
-    pub owner: Owner,
-    pub whitelist: Whitelist,
+    pub access_control: AccessControl,
     pub repository: Repository,
 }
 
 impl VariableRepositoryContractInterface for VariableRepositoryContract {
+    delegate! {
+        to self.access_control {
+            fn is_whitelisted(&self, address: Address) -> bool;
+            fn get_owner(&self) -> Option<Address>;
+            fn change_ownership(&mut self, owner: Address);
+            fn add_to_whitelist(&mut self, address: Address);
+            fn remove_from_whitelist(&mut self, address: Address);
+        }
+    }
+
     fn init(&mut self) {
         let deployer = caller();
-        self.owner.init(deployer);
-        self.whitelist.add_to_whitelist(deployer);
+        self.access_control.init(deployer);
         self.repository.init();
     }
 
-    fn change_ownership(&mut self, owner: Address) {
-        self.owner.ensure_owner();
-        self.owner.change_ownership(owner);
-        self.whitelist.add_to_whitelist(owner);
-    }
-
-    fn add_to_whitelist(&mut self, address: Address) {
-        self.owner.ensure_owner();
-        self.whitelist.add_to_whitelist(address);
-    }
-
-    fn remove_from_whitelist(&mut self, address: Address) {
-        self.owner.ensure_owner();
-        self.whitelist.remove_from_whitelist(address);
-    }
-
     fn update_at(&mut self, key: String, value: Bytes, activation_time: Option<u64>) {
-        self.whitelist.ensure_whitelisted();
+        self.access_control.ensure_whitelisted();
         self.repository.update_at(key, value, activation_time);
     }
 
@@ -72,14 +65,6 @@ impl VariableRepositoryContractInterface for VariableRepositoryContract {
 
     fn keys_count(&self) -> u32 {
         self.repository.keys.size()
-    }
-
-    fn get_owner(&self) -> Option<Address> {
-        self.owner.get_owner()
-    }
-
-    fn is_whitelisted(&self, address: Address) -> bool {
-        self.whitelist.is_whitelisted(&address)
     }
 }
 
