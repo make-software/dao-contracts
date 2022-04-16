2618c5826bc9134c38615c6ff3c6ff353a0e1fa7
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-04-16 16:50:48 +0200

    extract config to a function

--- dao-contracts/tests/test_onboarding_voter.rs
+++ dao-contracts/tests/test_onboarding_voter.rs
@@ -1,44 +1,19 @@
+use casper_dao_contracts::{
+    DaoOwnedNftContractTest, OnboardingVoterContractTest, ReputationContractTest,
+    VariableRepositoryContractTest,
+};
+use casper_dao_utils::{Address, TestEnv};
+use casper_types::U256;
 use speculate::speculate;
 
 speculate! {
-    use casper_dao_contracts::{OnboardingVoterContractTest, DaoOwnedNftContractTest, ReputationContractTest, VariableRepositoryContractTest};
     use casper_types::U256;
     use casper_dao_contracts::voting::onboarding;
     use casper_dao_utils::Error;
 
     before {
-        let env = casper_dao_utils::TestEnv::new();
         #[allow(unused_variables, unused_mut)]
-        let mut va_token = DaoOwnedNftContractTest::new(&env, "va token".to_string(), "vat".to_string(), "".to_string());
-        #[allow(unused_variables, unused_mut)]
-        let mut kyc_token = DaoOwnedNftContractTest::new(&env, "kyc token".to_string(), "kyt".to_string(), "".to_string());
-        let mut reputation_token = ReputationContractTest::new(&env);
-        let mut variable_repo = VariableRepositoryContractTest::new(&env);
-
-        let mint_amount = 10_000.into();
-        let vote_amount = 1_000.into();
-
-        #[allow(unused_variables, unused_mut)]
-        let mut contract = OnboardingVoterContractTest::new(
-            &env,
-            variable_repo.address(),
-            reputation_token.address(),
-            kyc_token.address(),
-            va_token.address(),
-        );
-        // Voter Contract becomes the owner of Variable Repo and Reputation Token
-        variable_repo
-                .change_ownership(contract.address())
-                .unwrap();
-
-        reputation_token
-                .change_ownership(contract.address())
-                .unwrap();
-        #[allow(unused_variables)]
-        let va = env.get_account(1);
-        let member = env.get_account(2);
-        // The voter has to have some tokens
-        reputation_token.mint(member, mint_amount).unwrap();
+        let (va, member, mint_amount, vote_amount, mut va_token, mut kyc_token, mut reputation_token, mut variable_repo, mut contract) = setup();
     }
 
     describe "voting" {
@@ -179,4 +154,68 @@ speculate! {
 
         }
     }
+}
+
+fn setup() -> (
+    Address,
+    Address,
+    U256,
+    U256,
+    DaoOwnedNftContractTest,
+    DaoOwnedNftContractTest,
+    ReputationContractTest,
+    VariableRepositoryContractTest,
+    OnboardingVoterContractTest,
+) {
+    let env = TestEnv::new();
+
+    let va_token = DaoOwnedNftContractTest::new(
+        &env,
+        "va token".to_string(),
+        "vat".to_string(),
+        "".to_string(),
+    );
+    let kyc_token = DaoOwnedNftContractTest::new(
+        &env,
+        "kyc token".to_string(),
+        "kyt".to_string(),
+        "".to_string(),
+    );
+    let mut reputation_token = ReputationContractTest::new(&env);
+    let mut variable_repo = VariableRepositoryContractTest::new(&env);
+
+    let onboarding_voter = OnboardingVoterContractTest::new(
+        &env,
+        variable_repo.address(),
+        reputation_token.address(),
+        kyc_token.address(),
+        va_token.address(),
+    );
+
+    // Voter Contract becomes the owner of Variable Repo and Reputation Token
+    variable_repo
+        .change_ownership(onboarding_voter.address())
+        .unwrap();
+
+    reputation_token
+        .change_ownership(onboarding_voter.address())
+        .unwrap();
+    let va = env.get_account(1);
+    let member = env.get_account(2);
+    // The voter has to have some tokens
+    let mint_amount = 10_000.into();
+    let vote_amount = 1_000.into();
+    reputation_token.mint(member, mint_amount).unwrap();
+
+    (
+        va,
+        member,
+        mint_amount,
+        vote_amount,
+        va_token,
+        kyc_token,
+        reputation_token,
+        variable_repo,
+        onboarding_voter,
+    )
 }
