f24f7597b18c88be9830d72fbccfe12e5fa8a41d
Piotr Witek <piotrek.witek@gmail.com>
2022-06-16 14:19:50 +0200

    Finished generic client with e2e-tests and documentation

---
+++ client/Untitled-1.sh
@@ -0,0 +1,4 @@
+casper-client query-global-state \
+        --node-address "$(get_node_address_rpc)" \
+        --state-root-hash "$(get_state_root_hash)" \
+        --key "hash-dcf25bb7e08aac7056d4afa124bd376de6783bf0184b43480fbbef4f8c5fa4a9"
