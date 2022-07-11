15c730bba6f87bb257498c7d496a47b6916ec259
Kuba Płaskonka <kuba.plaskonka@gmail.com>
2022-07-11 12:18:34 +0200

    Simple voter

---
+++ dao-contracts/tests/test_simple_voter.rs
@@ -0,0 +1,88 @@
+mod governance_voting_common;
+
+use speculate::speculate;
+
+use casper_dao_contracts::{simple_voter, voting::Choice, voting::VotingId};
+
+use casper_dao_utils::TestContract;
+use casper_types::U256;
+use simple_voter::DocumentHash;
+
+speculate! {
+    context "simple_voter" {
+        before {
+            let mut simple_voter_contract = governance_voting_common::setup_simple_voter();
+        }
+
+        context "with informal voting in progress" {
+            before {
+                simple_voter_contract.as_nth_account(0).create_voting(DocumentHash::from(123), U256::from(500)).unwrap();
+                let voting_id = VotingId::from(0);
+                #[allow(unused_variables)]
+                let voting = simple_voter_contract.get_voting(voting_id).unwrap();
+            }
+
+            test "document hash is saved in the contract" {
+                let document_hash = simple_voter_contract.get_document_hash(voting_id);
+                assert_eq!(document_hash, Some(DocumentHash::from(123)));
+            }
+
+            context "when informal voting fails" {
+                before {
+                    simple_voter_contract
+                        .as_nth_account(1)
+                        .vote(voting.voting_id(), Choice::Against, 1000.into())
+                        .unwrap();
+
+                    simple_voter_contract
+                        .as_nth_account(2)
+                        .vote(voting.voting_id(), Choice::Against, 1000.into())
+                        .unwrap();
+
+                    simple_voter_contract
+                        .advance_block_time_by(voting.informal_voting_time().unwrap() + 1);
+
+                    simple_voter_contract
+                        .as_nth_account(2)
+                        .finish_voting(voting.voting_id())
+                        .unwrap();
+                }
+
+                test "there is no new voting" {
+                    let formal_voting_id = VotingId::from(1);
+                    let document_hash = simple_voter_contract.get_document_hash(formal_voting_id);
+                    assert_eq!(document_hash, None);
+                }
+            }
+
+            context "when informal voting succeeds" {
+                before {
+                    simple_voter_contract
+                        .as_nth_account(1)
+                        .vote(voting.voting_id(), Choice::InFavor, 1000.into())
+                        .unwrap();
+
+                    simple_voter_contract
+                        .as_nth_account(2)
+                        .vote(voting.voting_id(), Choice::InFavor, 1000.into())
+                        .unwrap();
+
+                    simple_voter_contract
+                        .advance_block_time_by(voting.informal_voting_time().unwrap() + 1);
+
+                    simple_voter_contract
+                        .as_nth_account(2)
+                        .finish_voting(voting.voting_id())
+                        .unwrap();
+
+                    let formal_voting_id = VotingId::from(1);
+                }
+
+                test "there is a new voting with the same document hash" {
+                    let document_hash = simple_voter_contract.get_document_hash(formal_voting_id);
+                    assert_eq!(document_hash, Some(DocumentHash::from(123)));
+                }
+            }
+        }
+    }
+}
