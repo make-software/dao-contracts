62d4fb4fce8126519754948ed44527e609e2cc0d
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2022-11-23 10:15:43 +0100

    wip

--- dao-contracts/tests/features/kyc/kyc_nft_token.feature
+++ dao-contracts/tests/features/kyc/kyc_nft_token.feature
@@ -3,16 +3,32 @@ Feature: KYC Token
   Scenario: KYC Token initial state.
     Then total supply is 0 tokens
 
-  Rule: Only whitelisted user can mint a token.
+  Rule: Only whitelisted user can mint a KYC Token.  
     Background:
-      When the Owner adds Bob to the whitelist
-    
-    Scenario Outline: If <minter> mints a token, the balance is updated
-      When a <minter> mints a token to any user.
-      Then the balance is <balance>.
+      Given users
+        | user   | is_whitelisted |
+        | Alice  | false          |
+        | Bob    | true           |
+    Scenario Outline: If <minter> mints a KYC Token, the balance is updated
+      When <minter> mints a KYC Token to any user.
+      Then the user's balance is <balance>.
 
     Examples:
       | minter | balance |
       | Owner  | 1       |
       | Alice  | 0       |
       | Bob    | 1       |
+
+  Rule: Owner can mint the first token to a user.
+    Scenario: If Owner mints KYC Token to Bob, he's balance is updated.
+      When Owner mints a KYC Token to Bob.
+      Then the Bob's balance is 1.
+      And Token with id 0 belongs to Bob.
+
+  Rule: User can own only one KYC Token.
+    Background:
+      Given user Bob that owns a KYC Token.
+    
+    Scenario: If Owner mints the second KYC Token for Bob, he's balance remains the same.
+      When Owner mints a KYC Token to Bob.
+      Then the Bob's balance is 1.
--- dao-contracts/tests/steps/kyc.rs
+++ dao-contracts/tests/steps/kyc.rs
@@ -1,59 +1,86 @@
 use std::str::FromStr;
 
 use casper_dao_utils::{Address, TestContract};
-use casper_types::U256;
-use cucumber::{given, then, when, Parameter};
-
+use cucumber::{given, then, when, Parameter, gherkin::Step};
 
 use crate::common::DaoWorld;
 
-#[then(expr = "total supply is {int} tokens")]
-fn total_reputation(w: &mut DaoWorld, expected_total_supply: u32) {
-    let total_supply = w.kyc_token.total_supply();
-    assert_eq!(
-        total_supply,
-        U256::from(expected_total_supply)
-    );
+#[given(expr = "users")]
+fn whitelisting(world: &mut DaoWorld, step: &Step) {
+    let users_iter = step.table.as_ref().unwrap().rows.iter().skip(1);
+
+    for row in users_iter {
+        let account: Account = row[0].parse().unwrap();
+        let should_whitelist: bool = row[1].parse().unwrap();
+
+        if should_whitelist {
+            account.whitelist(world);
+        }
+    }
 }
 
-#[when(expr = "the {account} adds {account} to the whitelist")]
-fn whitelist(world: &mut DaoWorld, active_account: Account, candidate: Account) {
-    let active_account = active_account.get_address(world);
-    let candidate = candidate.get_address(world);
+#[given(expr = "user {account} that owns a KYC Token.")]
+fn setup_user_with_token(world: &mut DaoWorld, user: Account) {
+    user.whitelist(world);
+    user.mint(world, Account::Owner);
 
-    world.kyc_token
-        .as_account(active_account)
-        .add_to_whitelist(candidate)
-        .expect("{active_account} Should be added to the whitelist");
+    assert_eq!(user.balance(world), U256::one());
 }
 
-#[given(expr = "a {account}")]
-async fn setup(world: &mut DaoWorld, user: Account) {
-    let minter = user.get_address(world);
-    world.env.as_account(minter);
-}
-
-#[when(expr = "a {account} mints a token to any user.")]
-async fn mint(world: &mut DaoWorld, minter: Account) {
+#[when(expr = "{account} mints a KYC Token to {account}.")]
+async fn mint(world: &mut DaoWorld, minter: Account, recipient: Account) {
     let minter = minter.get_address(world);
-    let any_user = Account::Any.get_address(world);
+    let recipient = recipient.get_address(world);
 
     let _ = world.kyc_token
         .as_account(minter)
-        .mint(any_user);
+        .mint(recipient);
 }
 
 
-#[then(expr = "the balance is {int}.")]
-fn balance(world: &mut DaoWorld, expected_balance: u32) {
-    let any_user = Account::Any.get_address(world);
+#[then(expr = "the {account}'s balance is {u256}.")]
+fn assert_balance(world: &mut DaoWorld, user: Account, expected_balance: U256) {
+    assert_eq!(user.balance(world), expected_balance);
+}
 
-    let balance = world.kyc_token.balance_of(any_user);
-    assert_eq!(balance, expected_balance.into());
+#[then(expr = "Token with id {token} belongs to {account}.")]
+fn assert_token_ownership(world: &mut DaoWorld, token_id: TokenId, user: Account) {
+    let contract = &world.kyc_token;
+    let user = user.get_address(world);
+    
+    assert_eq!(
+        contract.owner_of(*token_id),
+        Some(user)
+    );
+
+    assert_eq!(
+        contract.token_id(user),
+        Some(0.into())
+    );
+}
+
+#[then(expr = "total supply is {u256} tokens")]
+fn assert_total_supply(world: &mut DaoWorld, expected_total_supply: U256) {
+    let total_supply = world.kyc_token.total_supply();
+    assert_eq!(total_supply, expected_total_supply.0);
+}
+
+#[derive(Debug, Default, derive_more::FromStr, derive_more::Deref, Parameter)]
+#[param(name = "token", regex = r"\d+")]
+struct TokenId(pub casper_dao_erc721::TokenId);
+
+#[derive(Debug, Default, derive_more::FromStr, derive_more::Deref, Parameter, PartialEq, Eq)]
+#[param(name = "u256", regex = r"\d+")]
+struct U256(pub casper_types::U256);
+
+impl U256 {
+    fn one() -> Self {
+        U256(casper_types::U256::one())
+    }
 }
 
 #[derive(Debug, Default, Parameter)]
-#[param(name = "account", regex = "Bob|Alice|Owner|user|any||")]
+#[param(name = "account", regex = "Bob|Alice|Owner|user|any|any user|")]
 enum Account {
     Alice,
     Bob,
@@ -71,6 +98,30 @@ impl Account {
             Account::Any => 3,
         };
         world.env.get_account(idx)
+    }
+
+    fn whitelist(&self, world: &mut DaoWorld) {
+        let owner = Account::Owner.get_address(world);
+        let user = self.get_address(world);
+
+        world.kyc_token
+            .as_account(owner)
+            .add_to_whitelist(user)
+            .expect("User should be added to the whitelist");
+    }
+
+    fn mint(&self, world: &mut DaoWorld, minter: Account) {
+        let minter = minter.get_address(world);
+        let user = self.get_address(world);
+
+        world.kyc_token
+            .as_account(minter)
+            .mint(user)
+            .expect("A token should be minted");
+    }
+
+    fn balance(&self, world: &DaoWorld) -> U256 {
+        U256(world.kyc_token.balance_of(self.get_address(world)))
     }
 }
 
