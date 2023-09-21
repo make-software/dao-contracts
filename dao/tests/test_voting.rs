1f0e7539fcbd19d97c95340b03129ce01b6118ac
Kuba Płaskonka <kuba@odra.dev>
2023-09-21 16:23:06 +0200

    Fixes after review

--- dao/tests/test_voting.rs
+++ dao/tests/test_voting.rs
@@ -7,6 +7,6 @@ use cucumber::World as _;
 fn main() {
     let runner = DaoWorld::cucumber()
         .with_runner(cucumber_runner::SyncRunner::default())
-        .run_and_exit("tests/features/voting/reputation/reputation_actions.feature");
+        .run_and_exit("tests/features/voting/");
     futures::executor::block_on(runner);
 }
